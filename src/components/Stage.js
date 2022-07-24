1090a1c7e90c079c7779b55295b10f780efc662e
Andrew Henin <ahenin1@swarthmore.edu>
2022-07-24 14:40:16 -0400

    push first files

---
+++ src/components/Stage.js
@@ -0,0 +1,14 @@
+import { type } from "@testing-library/user-event/dist/type";
+import React from "react";
+import Cell from './Cell';
+
+
+const Stage = ({ stage }) => (
+    <div>
+        {stage.map(row => row.map((cell, x) => <Cell key={x} type={Cell[0]}/>))}
+
+
+    </div>
+);
+
+export default Stage;
